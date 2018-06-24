a3b835bb18dda9990d32195922785e8f956b0d59
snsvrno <snsvrno@tuta.io>
2018-06-23 19:07:58 -0600

    adds ability to change folder and file where settings are

--- src/paths.rs
+++ src/paths.rs
@@ -6,11 +6,25 @@ use std::ffi;
 
 use ansi_term::Colour::Yellow;
 
+fn get_settings_file_name() -> String {
+  match env::var("LPSETTINGS_FILE") {
+    Err(_) => super::SETTINGS_FILE.to_string(),
+    Ok(val) => val.to_string(),
+  }
+}
+
+fn get_settings_folder_name() -> String {
+  match env::var("LPSETTINGS_USERFOLDER") {
+    Err(_) => super::SETTINGS_FOLDER.to_string(),
+    Ok(val) => val.to_string(),
+  }
+}
+
 pub fn get_global_settings_path() -> PathBuf {
   //! determines what the *global* file path should be.
 
   let mut home_dir = get_global_settings_folder();
-  home_dir.push(super::SETTINGS_FILE);
+  home_dir.push(get_settings_file_name());
   home_dir
 }
 
@@ -18,7 +32,7 @@ pub fn get_global_settings_folder() -> PathBuf {
   //! builds the *global* settings folder.
 
   let mut home_dir = env::home_dir().unwrap();
-  home_dir.push(super::SETTINGS_FOLDER);
+  home_dir.push(get_settings_folder_name());
   home_dir
 }
 
@@ -26,11 +40,11 @@ pub fn get_local_settings_path() -> PathBuf {
   //! determines what the *local* file path should be.
   
   if let Ok(mut dir) = env::current_dir() {
-    dir.push(super::SETTINGS_FILE);
+    dir.push(get_settings_file_name());
     dir
   } else {
     let mut path = Path::new(".").to_path_buf();
-    path.push(super::SETTINGS_FILE);
+    path.push(get_settings_file_name());
     path
   }
 }
